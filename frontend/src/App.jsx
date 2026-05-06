cddd0de991c3a949035f0b5c55a7d9aabbdd02f6
David Sabater <sabaterdavidp@gmail.com>
2026-05-06 14:51:31 +0200

    Instalación inicial del entorno cliente y conexión con servidor.
    Instalación inicial del entorno cliente y conexión con servidor.
    - Instalación de Axios
    - Creación de /routes/api.php (servidor)
    - Redirección de rutas de /bootstrap/app.php (withRouting)
    - Creacion de frontend con React
    - App.jsx con conexión de prueba al servidor

---
+++ frontend/src/App.jsx
@@ -0,0 +1,35 @@
+import './App.css'
+
+// Dependencias Axios para hacer peticiones HTTP
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+// LLamada al backend para obtener un mensaje de prueba y mostrarlo en la interfaz
+function App() {
+  const [message, setMessage] = useState('cargando...')
+  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    axios.get('http://localhost/GitHub/TigerGrit/backend/public/api/test')
+      .then(res => {
+        setMessage(res.data.message)
+      })
+      .catch(() => {
+        setMessage('error conectando con backend')
+      })
+  }, [])
+
+  return (
+    <div>
+      <h1>React + Laravel</h1>
+
+      <p>{message}</p>
+
+      <button onClick={() => setCount(count + 1)}>
+        Count: {count}
+      </button>
+    </div>
+  )
+}
+
+export default App
