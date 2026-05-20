ddf883c5edabb0bbbed31fa29d8b8693482d6b5b
David Sabater <sabaterdavidp@gmail.com>
2026-05-20 12:41:22 +0200

    Login y Register visuales
    Ahora la pagina principal del cliente permitira tanto crear una cuenta como iniciar sesion guardando el token en el historial del navegador

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,33 +4,91 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-// LLamada al backend para obtener un mensaje de prueba y mostrarlo en la interfaz
 function App() {
-  const [message, setMessage] = useState('cargando...')
-  const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    axios.get(`${import.meta.env.VITE_API_URL}/test`)
-      .then(res => {
-        setMessage(res.data.message)
-      })
-      .catch(error => {
-        console.error(error)
-        setMessage('error conectando con backend')
-      })
-  }, [])
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const login = async () => {
+      
+    try {
+
+      const response = await axios.post( // Se hace la petición POST al endpoint de login de tu backend
+        `${import.meta.env.VITE_API_URL}/login`,
+        {
+          email,
+          password
+        }
+      )
+
+      localStorage.setItem( // Se guarda el token en el localStorage del navegador
+        'token',
+        response.data.token
+      )
+
+      console.log(response.data)
+
+    } catch (error) {
+
+      console.error(error)
+
+    }
+  }
+
+  const getUser = async () => {
+
+  const token = localStorage.getItem('token')
+
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/user`,
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        )
+      
+        console.log(response.data)
+
+        } catch (error) {
+
+          console.error(error)
+
+        }
+    }
 
   return (
+
     <div>
-      <h1>React + Laravel</h1>
 
-      <p>{message}</p>
+      <h1>Login</h1>
 
-      <button onClick={() => setCount(count + 1)}>
-        Count: {count}
+      <input
+        type="email"
+        placeholder="email"
+        onChange={(e) => setEmail(e.target.value)}
+      />
+
+      <input
+        type="password"
+        placeholder="password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <button onClick={login}>
+        Login
       </button>
+
+
+      <button onClick={getUser}>
+        Obtener usuario
+      </button>
+
     </div>
   )
+
+  
 }
 
 export default App
